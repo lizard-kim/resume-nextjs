76a497a93ee752863d31d1c12f099832248e7cf2
lizard-kim <rlaxodud1122@gmail.com>
2020-06-18 06:12:24 +0900

    error fix2

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,6 +1,6 @@
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
-import { faGithub, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import image from '../asset/lizard_profile.jpg';
 import { IProfile } from '../component/profile/IProfile';
 
